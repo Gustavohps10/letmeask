93b39714e0e511ff97ef4926a31b1d0d2dcfcea6
gustavo <gustavoh.santos735@gmail.com>
2022-02-14 07:46:35 -0300

    NewRoom page

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/rooms/new" element={<NewRoom/>}/>
     </Routes>
   </BrowserRouter>
   );
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,34 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logo from '../assets/images/logo.svg'
+import googleIcon from  '../assets/images/google-icon.svg'
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss'
+
+export function NewRoom(){
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="illustration" />
+                <h1>Crie salas de Q&A ao vivo</h1>
+                <p>Tire as dúvidas da sua audiência em tempo real</p>
+            </aside>
+            <main>
+                <div className='main-content'>
+                    <img src={logo} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                    <form>
+                        <input 
+                            type="text" 
+
+                            placeholder='Nome da sala'
+                        />
+                        <br />
+                        <Button type="submit">Criar Sala</Button>
+                    </form>
+                    <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
+                </div>
+            </main>
+        </div>
+    )
+}
